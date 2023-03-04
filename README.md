eb587d6d8cfc852d34e295acd981b93e81a1ffbe
matt <bassetmatt@gmail.com>
2023-03-04 12:52:35 -0500

    Add gitignore, upgrade readme and configure base buildsystem

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 h
 
+## Build
+For now we use CMake (can change)<br>
+For now we use a global recursive method for more simplicity although it's commonly known as a bad pratice
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
-#include "../include/utils.hpp"
+#include <iostream>
+#include "utils.hpp"
 
 int main() {
+	std::cout << "A very respectble print test" << std::endl;
 	return 0;
 }
