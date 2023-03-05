ac56bc05e8c1a369315a6046cdb2156129733a15
anima-libera <anim.libera@gmail.com>
2023-03-05 02:45:04 +0100

    Minor insignificant style changes again

--- src/gameloop.cpp
+++ src/gameloop.cpp
@@ -4,19 +4,19 @@
 #include "events.hpp"
 
 namespace gameloop {
-	static bool gameloop_running = false;
+	static bool s_gameloop_running = false;
 
 	void loop() {
-		gameloop_running = true;
-		while (gameloop_running) {
+		s_gameloop_running = true;
+		while (s_gameloop_running) {
 			handle_events();
 			renderer::draw_all();
 		}
 	}
 
 	void exit() {
-		assert(gameloop_running
+		assert(s_gameloop_running
 			/* It makes no sense to exit from the game loop when it is not running. */);
-		gameloop_running = false;
+		s_gameloop_running = false;
 	}
 }
