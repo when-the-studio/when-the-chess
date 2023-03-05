702ce3ca0f6d2da7d4653118ddb1d10b937c80cf
anima-libera <anim.libera@gmail.com>
2023-03-05 18:50:12 +0100

    Made the build script exit (and not run the binary) if compilation failed

--- cmakebuild.sh
+++ cmakebuild.sh
@@ -3,7 +3,15 @@ build() {
     cd build
     cmake ..
     make wtc
+    compilation_result=$?
     cd ..
+
+    if [ $compilation_result -ne 0 ]
+    then
+        # Compilation failed, we probably want to stop here in that case.
+		echo "\e[31m\e[1mCompilation failed >.<\e[21m\e[39m"
+        exit 1
+    fi
 }
 
 
